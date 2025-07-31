a351233a5da5eaf8a5a94a0877553a71e9f6042f
Ajeet Singh <as7641012@gmail.com>
2025-07-31 18:45:53 +0530

    fixes 404 errors and blank screen

--- next.config.js
+++ next.config.js
@@ -7,6 +7,8 @@ const nextConfig = {
     ...(process.env.NODE_ENV === 'production' && {
         output: 'export',
         trailingSlash: true,
+        basePath: '/chess-game',
+        assetPrefix: '/chess-game/',
     }),
 
     images: {
